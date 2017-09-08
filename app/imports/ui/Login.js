f6c325990385790e074c3eb1e6355cf112143fcd
sk-falcon <sparsh.dtu@gmail.com>
2017-09-08 13:01:21 +0530

    - Login and Signup system setup

--- app/imports/ui/Login.js
+++ app/imports/ui/Login.js
@@ -1,8 +1,45 @@
 import React from 'react'
+import { Link } from 'react-router'
+import { Meteor } from 'meteor/meteor'
 
 export default class Login extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      error: ''
+    }
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    let email = this.refs.email.value.trim();
+    let password = this.refs.password.value.trim();
+
+    Meteor.loginWithPassword({email}, password, (err) => {
+      if(err) {
+        this.setState({error: 'Unable to login.'});
+      } else {
+        this.setState({error: ''});
+      }
+    });
+  }
+
+
   render() {
-    return <p>Login</p>;
+    return (
+      <div>
+        <h1>Login</h1>
+        {this.state.error ? <p>{this.state.error}</p> : undefined}
+        <form onSubmit={this.onSubmit.bind(this)} noValidate>
+          <input type="email" ref="email" placeholder="email"/>
+          <input type="password" ref="password" placeholder="password"/>
+          <button>Login</button>
+        </form>
+
+        <Link to="/signup">Don't have an account?</Link>
+      </div>
+    );
   }
 
 }
--- app/imports/ui/SignUp.js
+++ app/imports/ui/SignUp.js
@@ -1,8 +1,47 @@
 import React from 'react'
+import { Link } from 'react-router'
+import { Accounts } from 'meteor/accounts-base'
 
 export default class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: ''
+    }
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    let email = this.refs.email.value.trim();
+    let password = this.refs.password.value.trim();
+
+    Accounts.createUser({email, password}, (err) => {
+      if(err) {
+        this.setState({error: err.reason});
+      } else {
+        this.setState({error: ''});
+      }
+    });
+  }
+
+
   render() {
-    return <p>SignUp</p>;
+    return (
+
+      <div>
+        <h1>Sign Up</h1>
+        {this.state.error ? <p>{this.state.error}</p> : undefined}
+        <form onSubmit={this.onSubmit.bind(this)} noValidate>
+          <input type="email" ref="email" placeholder="email"/>
+          <input type="password" ref="password" placeholder="password"/>
+          <button>Sign Up</button>
+        </form>
+
+        <Link to="/">Already have an account?</Link>
+      </div>
+
+    );
   }
 
 }
